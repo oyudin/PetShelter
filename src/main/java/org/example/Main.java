07f4c71cbbcd149b1eaa60fa3293d46d12ff5882
saniayudin <olex.yudin@gmail.com>
2022-12-02 01:15:30 +0200

    hm16

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,28 @@
-package org.example;public class Main {
+package org.example;
+
+import org.example.service.PetService;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        PetService petService = new PetService();
+        Scanner scanner = new Scanner(System.in);
+
+        petService.deserializePets();
+        Message.greeting();
+
+        while (true) {
+            switch (scanner.next()) {
+                case "1" -> petService.addPet();
+                case "2" -> petService.getPets();
+                case "3" -> petService.takePet();
+                case "4" -> petService.serializePets();
+                case "exit" -> System.exit(0);
+                default -> System.out.println("Wrong input, try again: \n");
+
+            }
+        }
     }
 }
